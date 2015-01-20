7a9bcc7c59dcfac435cc123bc38b29080e9194d1
Matija Folnovic <mfolnovic@gmail.com>
2015-01-20 12:23:42 +0100

    interactive mode

--- Hash.hs
+++ Hash.hs
@@ -2,7 +2,10 @@ module Hash (runScript, runInteractive) where
 
 import Parsing.HashParser
 import Language.Commands (commands)
-import Language.Exec (runHashProgram)
+import Language.Exec (runHashProgram, ScriptState)
+
+import System.Directory
+import System.IO
 
 -- The top-level module. Connects parsing to execution and adds interaction
 -- with the user / reading from file.
@@ -10,11 +13,22 @@ import Language.Exec (runHashProgram)
 runScript :: FilePath -> IO ()
 runScript path = do
   script <- readFile path
-  let exprs = parseScript(script)
+  let exprs = parseScript script
   putStrLn $ unlines [show expr | expr <- exprs]
   _ <- runHashProgram commands (Left path) exprs
   return ()
 
 -- Communicates with the user and performs hash commands line by line
 runInteractive :: IO ()
-runInteractive = putStrLn "interactive"
+runInteractive = do
+  dir <- getCurrentDirectory
+  runInteractive' $ Left dir
+
+runInteractive' :: Either FilePath ScriptState -> IO ()
+runInteractive' state = do
+  putStr "$ "
+  hFlush stdout
+  line <- getLine
+  let exprs = parseScript line
+  state' <- runHashProgram commands state exprs
+  runInteractive' $ Right state'
