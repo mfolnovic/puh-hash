f5cba1534b40db49ae77c0c318a90b0137bc92c6
Matija Folnovic <mfolnovic@gmail.com>
2015-01-20 17:16:29 +0100

    hashrc

--- Hash.hs
+++ Hash.hs
@@ -1,10 +1,13 @@
 module Hash (runScript, runInteractive) where
 
+import Control.Applicative
+
 import Parsing.HashParser
 import Language.Commands (commands)
-import Language.Exec (runHashProgram, ScriptState)
+import Language.Exec (runHashProgram, ScriptState(..))
 
 import System.Directory
+import System.FilePath
 import System.IO
 
 -- The top-level module. Connects parsing to execution and adds interaction
@@ -12,17 +15,17 @@ import System.IO
 -- Runs a .hash script
 runScript :: FilePath -> IO ()
 runScript path = do
-  script <- readFile path
-  let exprs = parseScript script
-  putStrLn $ unlines [show expr | expr <- exprs]
-  _ <- runHashProgram commands (Left path) exprs
+  dir <- getCurrentDirectory
+  state <- loadHashRc path
+  _ <- runPath dir state
   return ()
 
 -- Communicates with the user and performs hash commands line by line
 runInteractive :: IO ()
 runInteractive = do
   dir <- getCurrentDirectory
-  runInteractive' $ Left dir
+  state <- loadHashRc dir
+  runInteractive' state
 
 runInteractive' :: Either FilePath ScriptState -> IO ()
 runInteractive' state = do
@@ -32,3 +35,18 @@ runInteractive' state = do
   let exprs = parseScript line
   state' <- runHashProgram commands state exprs
   runInteractive' $ Right state'
+
+loadHashRc :: FilePath -> IO (Either FilePath ScriptState)
+loadHashRc path = do
+  hashRcPath <- fmap (\x -> combine x ".hashrc") getHomeDirectory
+  fileExists <- doesFileExist hashRcPath
+  if fileExists then do state <- runPath hashRcPath $ Left path
+                        return $ Right state
+                else return $ Left path
+
+runPath :: FilePath -> Either FilePath ScriptState -> IO ScriptState
+runPath path state = do
+  script <- readFile path
+  let exprs = parseScript script
+  putStrLn $ unlines [show expr | expr <- exprs]
+  runHashProgram commands state exprs
